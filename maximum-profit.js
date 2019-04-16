ff79dc83eb90c2879b099f3c1c2086ff73f45306
Scott Gall <scottarthurgall@gmail.com>
2019-04-15 23:08:46 -0500

    added comment

--- maximum-profit.js
+++ maximum-profit.js
@@ -1,30 +1,43 @@
-function getMaxProfit(arr) {
-  var minIdx = 0;
-  var maxIdx = 0;
-  var currMin = 0;
-  var maxProfit = 0;
+// function getMaxProfit(arr) {
+//   var minIdx = 0;
+//   var maxIdx = 1;
+//   var currMin = 0;
+//   var maxProfit = 0;
 
-  if(arr.length < 2) {
-    throw new Error('Need at lease two time periods to be profitable!');
-  };
+//   if(arr.length < 2) {
+//     throw new Error('Need at lease two time periods to be profitable!');
+//   };
 
-  for(var i = 1; i < arr.length; i++) {
-    // new current min
-    if(arr[i] < arr[currMin]) {
-      currMin = i;
-    };
+//   for(var i = 1; i < arr.length; i++) {
+//     // new current min
+//     if(arr[i] < arr[currMin]) {
+//       currMin = i;
+//     };
 
-    // new best profit
-    if(arr[maxIdx] - arr[minIdx] < arr[i] - arr[currMin]) {
-      maxIdx = i;
-      minIdx = currMin;
-    }
-  }
+//     // new best profit
+//     if(arr[maxIdx] - arr[minIdx] < arr[i] - arr[currMin]) {
+//       maxIdx = i;
+//       minIdx = currMin;
+//     }
+//   }
 
-  maxProfit = arr[maxIdx] - arr[minIdx];
-  return maxProfit;
+//   maxProfit = arr[maxIdx] - arr[minIdx];
+//   return maxProfit;
+// }
+
+// var arr1 = [10, 7, 2];
+// var arr2 = [1];
+// console.log(getMaxProfit(arr1));
+
+// returns -1 if no profit to be made
+function maxProfit(arr) {
+  var max_profit = -1;
+  var buy_price = 0;
+  var sell_price = 0;
+
+  var change_buy_index = true;
+
+  for (var)
 }
 
-var arr1 = [10, 7];
-var arr2 = [1];
-console.log(getMaxProfit(arr1));
+//comment
